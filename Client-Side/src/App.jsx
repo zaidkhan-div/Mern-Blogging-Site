8685b75749e76121ae82625e2a2de000fbc3f5fb
zaidkhan <zaidkhan@kali.kali>
2025-03-24 00:58:16 +0500

    create pages and routes

--- Client-Side/src/App.jsx
+++ Client-Side/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Signin from './Pages/SignIn'
+import Signup from './Pages/SignUp'
+import Dashboard from './Pages/Dashboard'
+import Projects from './Pages/Projects'
 
 const App = () => {
   return (
-    <div className='text-red-400'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
