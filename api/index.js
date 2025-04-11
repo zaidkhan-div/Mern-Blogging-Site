66cac054324360a1c5da6a8facb2d5c12e514f8c
zaidkhan-div <zaidscestudent@gmail.com>
2025-04-11 17:54:40 +0500

    connection with mongodb is done

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://zaidkhan:zaidkhan@mern-blog.tngdpp0.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog')
+    .then(() => {
+        console.log('MongoDb is connected');
+    })
+    .catch((error) => {
+        console.log(error);
+    })
 
 const app = express();
 
