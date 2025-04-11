4328d47ad973f8169f6cb362f8d19a3299de0713
zaidkhan-div <zaidscestudent@gmail.com>
2025-04-11 16:03:42 +0500

    create and run the server

--- Client-Side/src/App.jsx
+++ Client-Side/src/App.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import About from './Pages/About'
+import Projects from './Pages/Projects'
 import SignIn from './Pages/SignIn'
 import SignUp from './Pages/SignUp'
 import Dashboard from './Pages/Dashboard'
+import Header from './Components/Header'
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects/>} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/dashboard' element={<Dashboard />} />
