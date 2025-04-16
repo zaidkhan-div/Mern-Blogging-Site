2746704514b8f869b154e3a952ca203251e15443
zaidkhan-div <zaidscestudent@gmail.com>
2025-04-16 10:53:52 +0500

    added functionality to sign-up page

--- Client-Side/src/Pages/SignUp.jsx
+++ Client-Side/src/Pages/SignUp.jsx
@@ -1,27 +1,45 @@
-import { Button, Label, TextInput } from 'flowbite-react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
 
-  const [formData, setFormData] = useState({})
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate()
 
   const handChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   }
 
 
   const handeSubmit = async (e) => {
     e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all the fields.')
+    }
     try {
+      setLoading(true);
+      setErrorMessage(null)
       const res = await fetch('/api/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-    } catch (error) {
 
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false)
+      if (res.ok) {
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setLoading(false)
     }
 
   }
@@ -75,17 +93,31 @@ const SignUp = () => {
                 onChange={handChange}
               />
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit'>
-              Sign Up
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {
+                loading ? (
+                  <>
+                    < Spinner siz='sm' />
+                    <span className='pl-3'>Loadin...</span>
+                  </>
+                ) : "Sign Up"
+              }
             </Button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
             <Link to='/sign-in' className='text-blue-500'>Sign In</Link>
           </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            ) // is not null
+          }
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
