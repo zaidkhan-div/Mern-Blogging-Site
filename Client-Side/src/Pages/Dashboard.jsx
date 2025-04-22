ca299a89cbc3f695b2bc1d4fd3176a348cd14f0f
zaidkhan-div <zaidscestudent@gmail.com>
2025-04-22 23:38:43 +0500

    Dashboad

--- Client-Side/src/Pages/Dashboard.jsx
+++ Client-Side/src/Pages/Dashboard.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import DashSidebar from '../Components/DashSidebar';
+import DashProfile from '../Components/DashProfile';
 
 const Dashboard = () => {
+
+  const location = useLocation();
+  const [tab, setTab] = useState();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    // console.log(tabFromUrl);
+    if (tabFromUrl) {
+      setTab(tabFromUrl)
+    }
+  }, [Location.search]);
+
+
   return (
-    <div>Dashboard</div>
+    <>
+      <div className='min-h-screen flex flex-col md:flex-row'>
+        <div className='md:w-56'>
+          {/* Sidebar */}
+          <DashSidebar />
+        </div>
+        {/* profle... */}
+
+        {tab === 'profile' && <DashProfile />}
+      </div>
+    </>
   )
 }
+
 
 export default Dashboard
